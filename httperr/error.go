1251027f76d4a85a70f5a155b1bed5eb68d8bada
Nick Stogner <nicholas.stogner@gmail.com>
2016-02-08 09:25:55 -0500

    Add xml annotations to httperr.Err

--- httperr/error.go
+++ httperr/error.go
@@ -1,9 +1,9 @@
 package httperr
 
 type Err struct {
-	StatusCode int                    `json:"-"`
-	Message    string                 `json:"message"`
-	Fields     map[string]interface{} `json:"fields"`
+	StatusCode int                    `json:"-" xml:"-"`
+	Message    string                 `json:"message" xml:"message"`
+	Fields     map[string]interface{} `json:"fields" xml:"fields"`
 }
 
 func (err Err) Error() string {
