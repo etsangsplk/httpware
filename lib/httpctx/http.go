e290babf07c852acb87f79cdc016d237f6beea55
Nick Stogner <nicholas.stogner@gmail.com>
2016-02-12 20:55:24 -0500

    Add alice-style chaining function

--- lib/httpctx/http.go
+++ lib/httpctx/http.go
@@ -24,3 +24,20 @@ type HandlerFunc func(context.Context, http.ResponseWriter, *http.Request) error
 func (h HandlerFunc) ServeHTTPContext(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	return h(ctx, w, r)
 }
+
+type Constructor func(Handler) Handler
+
+type Composite struct {
+	middle []Constructor
+}
+
+func Compose(cons ...Constructor) Composite {
+	return Composite{append(([]Constructor)(nil), cons...)}
+}
+
+func (c Composite) Then(h Handler) Handler {
+	for i := len(c.middle) - 1; i >= 0; i-- {
+		h = c.middle[i](h)
+	}
+	return h
+}
